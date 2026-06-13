e479bbdf02ac90ad116ec9c9c09e17849d6e520a
hooniegit <neivekim76@gmail.com>
2026-06-13 14:41:12 +0900

    base

---
+++ src/test/java/com/resourcebox/Disruptors/DisruptorsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.resourcebox.Disruptors;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DisruptorsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
